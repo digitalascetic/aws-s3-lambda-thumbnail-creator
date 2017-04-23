bba39a5ddde96d89bdddfda08a47225ce65c82b3
Martino Piccinato <mp@yoolab.com>
2017-04-24 01:02:54 +0200

    Commented sample configuration

--- configuration.sample.js
+++ configuration.sample.js
@@ -1,15 +1,22 @@
 exports.configuration = {
 
     /**
-     * 
+     * Generate a 150x150 px thumbnail
+     * with a -150x150 (.jpg) postfix
+     * for all files whose path starts with "public"
      */
     thumb150x150: {
         width: 150,
         height: 150,
         postfix: '-150x150',
-        fileMatch: /^(?!tmp)/gi
+        fileMatch: /^public/gi
     },
 
+    /**
+     * Default configuration will allow listed files types
+     * create the thumbnail with "public-read" acl, a quality ok 90
+     * and line interlace
+     */
     default: {
         allowedFileTypes: ['png', 'jpg', 'jpeg', 'bmp', 'tiff', 'pdf', 'gif'],
         acl: 'public-read',
